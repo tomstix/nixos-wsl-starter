6d5be74df36ce9467359019c2a7794a8bb7e84cf
Tom Stirnkorb <tom@stirnkorb.me>
2024-03-18 15:18:59 +0000

    Switch to p10k

--- home.nix
+++ home.nix
@@ -3,12 +3,12 @@
   # secrets,
   config,
   pkgs,
+  lib,
   username,
   nix-index-database,
   ...
 }: let
   unstable-packages = with pkgs.unstable; [
-    # FIXME: select your core binaries that you always want on the bleeding-edge
     bat
     bottom
     coreutils
@@ -22,7 +22,6 @@
     htop
     jq
     killall
-    lunarvim
     mosh
     neovim
     procs
@@ -34,6 +33,8 @@
     vim
     wget
     zip
+
+    go_1_22
   ];
 
   stable-packages = with pkgs; [
@@ -45,7 +46,7 @@
 
     # core languages
     rustup
-    go
+    #go
     lua
     nodejs
     python3
@@ -97,7 +98,7 @@ in {
     username = "${username}";
     homeDirectory = "/home/${username}";
 
-    sessionVariables.EDITOR = "lvim";
+    sessionVariables.EDITOR = "nvim";
     # set your preferred $SHELL
     sessionVariables.SHELL = "/etc/profiles/per-user/${username}/bin/zsh";
   };
@@ -112,9 +113,6 @@ in {
       # pkgs.unstable.some-other-package
     ];
 
-  # FIXME: if you want to version your LunarVim config, add it to the root of this repo and uncomment the next line
-  # home.file.".config/lvim/config.lua".source = ./lvim_config.lua;
-
   programs = {
     home-manager.enable = true;
     nix-index.enable = true;
@@ -122,20 +120,20 @@ in {
     nix-index-database.comma.enable = true;
 
     # FIXME: disable this if you don't want to use the starship prompt
-    starship.enable = true;
-    starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
-      git_branch.style = "242";
-      directory.style = "blue";
-      directory.truncate_to_repo = false;
-      directory.truncation_length = 8;
-      python.disabled = true;
-      ruby.disabled = true;
-      hostname.ssh_only = false;
-      hostname.style = "bold green";
-    };
+    # starship.enable = true;
+    # starship.settings = {
+    #   aws.disabled = true;
+    #   gcloud.disabled = true;
+    #   kubernetes.disabled = false;
+    #   git_branch.style = "242";
+    #   directory.style = "blue";
+    #   directory.truncate_to_repo = false;
+    #   directory.truncation_length = 8;
+    #   python.disabled = true;
+    #   ruby.disabled = true;
+    #   hostname.ssh_only = false;
+    #   hostname.style = "bold green";
+    # };
 
     # FIXME: disable whatever you don't want
     fzf.enable = true;
@@ -191,13 +189,16 @@ in {
       autocd = true;
       enableAutosuggestions = true;
       enableCompletion = true;
-      defaultKeymap = "emacs";
+      # defaultKeymap = "vicmd";
       history.size = 10000;
       history.save = 10000;
       history.expireDuplicatesFirst = true;
       history.ignoreDups = true;
       history.ignoreSpace = true;
       historySubstringSearch.enable = true;
+
+      initExtra = ''
+      '';
 
       plugins = [
         {
@@ -214,6 +215,16 @@ in {
             sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
           };
         }
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./p10k-config;
+          file = "p10k.zsh";
+        }
       ];
 
       shellAliases = {
@@ -221,20 +232,7 @@ in {
         "...." = "././..";
         cd = "z";
         gc = "nix-collect-garbage --delete-old";
-        refresh = "source ${config.home.homeDirectory}/.zshrc";
-        show_path = "echo $PATH | tr ':' '\n'";
-
-        # FIXME: add more git aliases here if you want them
-        gapa = "git add --patch";
-        grpa = "git reset --patch";
-        gst = "git status";
-        gdh = "git diff HEAD";
-        gp = "git push";
-        gph = "git push -u origin HEAD";
-        gco = "git checkout";
-        gcob = "git checkout -b";
-        gcm = "git checkout master";
-        gcd = "git checkout develop";
+	update = "sudo nixos-rebuild switch";
 
         pbcopy = "/mnt/c/Windows/System32/clip.exe";
         pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
@@ -243,87 +241,6 @@ in {
 
       envExtra = ''
         export PATH=$PATH:$HOME/.local/bin
-      '';
-
-      initExtra = ''
-        bindkey '^p' history-search-backward
-        bindkey '^n' history-search-forward
-        bindkey '^e' end-of-line
-        bindkey '^w' forward-word
-        bindkey "^[[3~" delete-char
-        bindkey ";5C" forward-word
-        bindkey ";5D" backward-word
-
-        zstyle ':completion:*:*:*:*:*' menu select
-
-        # Complete . and .. special directories
-        zstyle ':completion:*' special-dirs true
-
-        zstyle ':completion:*' list-colors ""
-        zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-
-        # disable named-directories autocompletion
-        zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
-
-        # Use caching so that commands like apt and dpkg complete are useable
-        zstyle ':completion:*' use-cache on
-        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-
-        # Don't complete uninteresting users
-        zstyle ':completion:*:*:*:users' ignored-patterns \
-                adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
-                clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
-                gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
-                ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
-                named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
-                operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
-                rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
-                usbmux uucp vcsa wwwrun xfs '_*'
-        # ... unless we really want to.
-        zstyle '*' single-ignored complete
-
-        # https://thevaluable.dev/zsh-completion-guide-examples/
-        zstyle ':completion:*' completer _extensions _complete _approximate
-        zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
-        zstyle ':completion:*' group-name ""
-        zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
-        zstyle ':completion:*' squeeze-slashes true
-        zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-        # mkcd is equivalent to takedir
-        function mkcd takedir() {
-          mkdir -p $@ && cd ''${@:$#}
-        }
-
-        function takeurl() {
-          local data thedir
-          data="$(mktemp)"
-          curl -L "$1" > "$data"
-          tar xf "$data"
-          thedir="$(tar tf "$data" | head -n 1)"
-          rm "$data"
-          cd "$thedir"
-        }
-
-        function takegit() {
-          git clone "$1"
-          cd "$(basename ''${1%%.git})"
-        }
-
-        function take() {
-          if [[ $1 =~ ^(https?|ftp).*\.(tar\.(gz|bz2|xz)|tgz)$ ]]; then
-            takeurl "$1"
-          elif [[ $1 =~ ^([A-Za-z0-9]\+@|https?|git|ssh|ftps?|rsync).*\.git/?$ ]]; then
-            takegit "$1"
-          else
-            takedir "$@"
-          fi
-        }
-
-        WORDCHARS='*?[]~=&;!#$%^(){}<>'
-
-        # fixes duplication of commands when using tab-completion
-        export LANG=C.UTF-8
       '';
     };
   };
